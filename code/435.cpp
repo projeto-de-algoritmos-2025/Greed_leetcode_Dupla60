0c65ef2e8ea9d28de82ec1b4850c49347308835c
matheus-06 <matheus2006.pinheiro@gmail.com>
2025-10-13 00:31:20 -0300

    Create 435.cpp

---
+++ code/435.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    /*
+    Posso fazer o Interval Schedueling normal, mas quando encontrar um evento incompativel, removo-o
+    1° ordenar em ordem crescente de termino
+    2° para cada tarefa verifico se ela é compatível, se for, atualiza o valor de end[i], se não é removida
+    */
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const vector<int>& a, const vector<int>& b) {
+            return a[1] < b[1];
+        });
+        // for(auto x : intervals){
+        //     cout << "[" << x[0] << " - " << x[1] << "]\n";
+        // }
+        int ans = 0;
+        int end = 0, start = 0;
+
+        for(int i = 0; i < intervals.size(); i++){
+            if(intervals[i][0] >= end){
+                end = intervals[i][1];
+            }
+            else
+                ans++;
+        }
+
+        return ans;
+    }
+};
